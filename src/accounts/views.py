4cc69e84d661f39ae13ae2c49d9a16cd6dcd6e17
Andrew <andrew.kennedy27@outlook.com>
2019-10-02 23:29:12 +0200

    added accounts app, register, login, profile & logout views

---
+++ src/accounts/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages, auth
+from .forms import UserLoginForm, UserRegistrationForm
+from django.template.context_processors import csrf
+from django.contrib.auth.decorators import login_required
+
+
+def register(request):
+    """Renders the registration form"""
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+
+            user = auth.authenticate(request.POST.get('email'),
+                                     password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+
+            else:
+                messages.error(request, "unable to log you in at this time!")
+    else:
+        user_form = UserRegistrationForm()
+
+    args = {'user_form': user_form}
+    return render(request, 'register.html', args)
+
+def login(request):
+    """Renders the login form"""
+    if request.method == 'POST':
+        user_form = UserLoginForm(request.POST)
+        if user_form.is_valid():
+            user = auth.authenticate(request.POST['username_or_email'],
+                                     password=request.POST['password'])
+
+            if user:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+
+                if request.GET and request.GET['next'] !='':
+                    next = request.GET['next']
+                    return HttpResponseRedirect(next)
+                else:
+                    return redirect(reverse('index'))
+            else:
+                user_form.add_error(None, "Your username or password are incorrect")
+    else:
+        user_form = UserLoginForm()
+
+    args = {'user_form': user_form, 'next': request.GET.get('next', '')}
+    return render(request, 'login.html', args)
+
+@login_required
+def profile(request):
+    """Renders user profile page"""
+    return render(request, 'profile.html')
+
+
+def logout(request):
+    """Logs user out and redirects to index page"""
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect(reverse('index'))
+
+
+
+
+
+
+
