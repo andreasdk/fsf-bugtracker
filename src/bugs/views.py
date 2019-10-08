1ece04f1ed6f154afe246019c0cba933db99c151
Andrew <andrew.kennedy27@outlook.com>
2019-10-09 01:57:22 +0200

    redirect to profile after new bug created

--- src/bugs/views.py
+++ src/bugs/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect, reverse
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages, auth
 from .forms import BugForm
+from .models import Bug
 from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 
@@ -9,17 +9,35 @@ from django.contrib.auth.decorators import login_required
 def new_bug(request):
     template_name='bugs/new.html'
     if request.method == 'POST':
-        bug_form = BugFormForm(request.POST)
+        bug_form = BugForm(request.POST or None, request.FILES or None)
         if bug_form.is_valid():
             bug = form.save(commit=False)
             bug.author = request.user
-            bug_form.save()
+            bug = form.save()
             messages.success(
                 request, "Thanks, your bug report has been submitted")
-            return redirect(reverse('index'))
+            return redirect(reverse('profile'))
     else:
         bug_form = BugForm()
 
     context= {'bug_form': bug_form}
     return render(request, template_name, context)
 
+@login_required
+def edit_bug(request, pk):
+    template_name='bugs/edit.html'
+    bug = get_object_or_404(Bug, pk)
+    if request.method == 'POST':
+        bug_form = BugForm(request.POST, instance=bug)
+        if bug_form.is_valid():
+            bug = form.save(commit=False)
+            bug.author = request.user
+            bug_form.save()
+            messages.success(
+                request, "Thanks, your bug report has been updated")
+            return redirect(reverse('index'))
+    else:
+        bug_form = BugForm(instance=bug)
+
+    context= {'bug_form': bug_form}
+    return render(request, template_name, context)
