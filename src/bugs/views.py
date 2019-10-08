3afc9a3385ac8aa843104b02f530ab725ed4c4c3
Andrew <andrew.kennedy27@outlook.com>
2019-10-08 22:03:11 +0200

    added bug creation view

--- src/bugs/views.py
+++ src/bugs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages, auth
+from .forms import BugForm
+from django.template.context_processors import csrf
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def new_bug(request):
+    template_name='bugs/new.html'
+    if request.method == 'POST':
+        bug_form = BugFormForm(request.POST)
+        if bug_form.is_valid():
+            bug = form.save(commit=False)
+            bug.author = request.user
+            bug_form.save()
+            messages.success(
+                request, "Thanks, your bug report has been submitted")
+            return redirect(reverse('index'))
+    else:
+        bug_form = BugForm()
+
+    context= {'bug_form': bug_form}
+    return render(request, template_name, context)
+
