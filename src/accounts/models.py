86070ed483246d422dbeb2ecc8c996b77a3d0662
Andrew <andrew.kennedy27@outlook.com>
2019-10-09 20:52:28 +0200

    add profile model, profile image resizing, default image location

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from PIL import Image
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(
+        default='default.jpg', 
+        upload_to='profile_pics',
+        blank=True,
+        null=True)
+
+    def __str__(self):
+        return '{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (256, 256)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+def create_profile(sender, created, instance, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_profile, sender=User)
