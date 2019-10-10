116acc7e1c0482a7b01cd0c9eb12dee0fc6b46d6
Andrew <andrew.kennedy27@outlook.com>
2019-10-10 18:04:55 +0200

    added view all bugs page

--- src/bugs/urls.py
+++ src/bugs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import new_bug
+from .views import new_bug, edit_bug, view_all
 
 
 urlpatterns = [
+    path('', view_all, name='view_all_bugs'),
     path('new/', new_bug, name='new_bug'),
+    path('edit/', edit_bug, name='edit_bug')
 ]
