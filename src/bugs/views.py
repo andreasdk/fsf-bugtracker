43356d17e779d2dace727ebf2afbbdd585b96d04
Andrew <andrew.kennedy27@outlook.com>
2019-10-11 23:17:16 +0200

    added single bug view, comment form

--- src/bugs/views.py
+++ src/bugs/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages, auth
-from .forms import BugForm
-from .models import Bug
+from .forms import BugForm, BugCommentForm
+from .models import Bug, BugComment
 from django.template.context_processors import csrf
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create new bug view
 @login_required
@@ -50,11 +51,38 @@ def view_all(request):
     template_name = 'bugs/view_all.html'
     ordering = ['-date_posted']
     paginate_by = 8
-
     context= {
         'bugs': bugs
     }
+    return render(request, template_name, context)
 
+# Single bug view
+def view_one(request, pk):
+    template_name = 'bugs/view_one.html'
+    bug = get_object_or_404(Bug, pk=pk)
+    comments = BugComment.objects.filter(bug_id=bug.pk)
+    user = User.objects.get(email=request.user.email)
+
+    if request.method == 'POST':
+        comment_form = BugCommentForm(request.POST or None)
+        if comment_form.is_valid():
+            comment_form.instance.author = request.user
+            comment = comment_form.save(commit=False)
+            comment.bug = bug
+            comment.save()
+            messages.success(
+                request, "Thanks, your comment has been submitted for moderation")
+            return redirect('view_one', pk=bug.pk)
+    else:
+        comment_form = BugCommentForm()
+        bug.views += 1
+        bug.save()
     
-
+    
+    context= {
+        'bug': bug,
+        'comment_form': comment_form,
+        'comments': comments,
+        'user': user
+    }
     return render(request, template_name, context)
