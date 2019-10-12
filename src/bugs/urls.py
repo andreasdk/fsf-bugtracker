af3939f94cc64d3759e5a42202e1db093474e0d7
Andrew <andrew.kennedy27@outlook.com>
2019-10-12 22:06:39 +0200

    added delete and vote urls

--- src/bugs/urls.py
+++ src/bugs/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, include
-from .views import new_bug, edit_bug, view_all, view_one
+from .views import (
+    new_bug, 
+    edit_bug, 
+    view_all, 
+    view_one, 
+    delete_bug,
+    vote,
+    )
 
 
 urlpatterns = [
     path('', view_all, name='view_all_bugs'),
     path('new/', new_bug, name='new_bug'),
-    path('edit/', edit_bug, name='edit_bug'),
+    path('<int:pk>/edit/', edit_bug, name='edit_bug'),
     path('<int:pk>/', view_one, name='view_one'),
+    path('<int:pk>/delete/', delete_bug, name='delete_bug'),
+    path('vote/<int:pk>', vote, name='vote'),
 ]
