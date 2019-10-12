3f9210d178614eda17ac8945713e8170a4332671
Andrew <andrew.kennedy27@outlook.com>
2019-10-12 22:02:03 +0200

    added comment and bug votes

--- src/bugs/admin.py
+++ src/bugs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Bug, BugComment
+from .models import Bug, BugComment, BugVotes
 
 # Register your models here.
 admin.site.register(Bug)
 admin.site.register(BugComment)
+admin.site.register(BugVotes)
