d980763caa512925cb3aa5ef5b57b3fffa3fe890
Andrew <andrew.kennedy27@outlook.com>
2019-10-19 00:36:31 +0200

    added django debug toolbar

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from accounts import urls as account_urls
 from bugs import urls as bug_urls
 from features import urls as feature_urls
@@ -24,6 +24,14 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+        path('', index, name="index"),
+        path('account/', include(account_urls)),
+        path('bugs/', include(bug_urls)),
+        path('features/', include(feature_urls)),
+        path('cart/', include(cart_urls)),
+        path('checkout/', include(checkout_urls)),
+        path('admin/', admin.site.urls),
+    ]
