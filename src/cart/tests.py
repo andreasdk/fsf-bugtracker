8eb68162d27a715559384cbe4888866dc5f40d28
Andrew <andrew.kennedy27@outlook.com>
2019-10-22 16:50:51 +0200

    added cart view test

--- src/cart/tests.py
+++ src/cart/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from features.models import Feature
 
-# Create your tests here.
+
+# Cart View Tests
+class TestRegisterView(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('12345')
+        user.save()
+        self.client = Client()
+        self.client.login(username='testuser', password='12345')
+
+    def test_cart_view(self):
+        page = self.client.get('/cart/')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'cart/cart.html')
+        
+        page = self.client.get('/carts/')
+        self.assertEqual(page.status_code, 404)
+        self.assertTemplateNotUsed(page, 'cart/cart.html')
+
